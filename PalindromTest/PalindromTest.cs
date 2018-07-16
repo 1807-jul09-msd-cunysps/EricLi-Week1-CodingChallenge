b41070d5ceb5299cca162d2fb6c90e3cebffee2e
TSLAnchoR <eli5407@bths.edu>
2018-07-16 12:51:42 -0400

    Project Upload

---
+++ PalindromTest/PalindromTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromTest
+{
+    public static class Palindrom
+    {
+        public static bool Test(string palindrome)
+        {
+            if (palindrome.Length <= 1)
+            {
+                return true;
+            }
+            else if (palindrome[0].Equals(palindrome[palindrome.Length - 1]))
+            {
+                return Test(palindrome.Substring(1, palindrome.Length - 2));
+            }
+            else
+                return false;
+        }
+    }
+}
---
+++ PalindromeTest/PalindromeTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PalindromTest;
+
+namespace PalimdromeTest
+{
+    [TestClass]
+    public class PalindromeTest
+    {
+        [TestMethod]
+        public void TestMethod0()
+        {
+            string testObject = "A nut for a jar of tuna.";
+            Assert.IsTrue(Palindrom.Test(testObject.Trim().ToLower().Trim()));
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string testObject = "Borrow or rob";
+            Assert.IsTrue(Palindrom.Test(testObject.ToLower().Trim()));
+
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string testObject = "343";
+            Assert.IsTrue(Palindrom.Test(testObject.Trim().ToLower().Trim()));
+
+        }
+    }
+}
